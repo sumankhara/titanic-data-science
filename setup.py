cad64384d49fef21ba97edfdac8b7c1a766a5399
Suman Khara <suman.khara@cerner.com>
2018-04-25 13:02:48 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='titanic data science challenge',
+    author='Suman Khara',
+    license='MIT',
+)
